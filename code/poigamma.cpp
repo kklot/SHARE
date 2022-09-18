8cbccd684aeff30eda531175f52e69f49ffaab06
Kinh Nguyen <4459378+kklot@users.noreply.github.com>
2022-09-18 21:32:28 +0200

    working model - re(yob)

---
+++ code/poigamma.cpp
@@ -0,0 +1,75 @@
+#include <TMB.hpp>
+#include "ktools.hpp"
+
+template <class Type>
+Type objective_function<Type>::operator()()
+{
+  parallel_accumulator<Type> dll(this);
+
+  // data
+  DATA_VECTOR(y);
+  DATA_VECTOR(E);
+  DATA_MATRIX(X);
+  DATA_VECTOR(I);
+
+  DATA_VECTOR(betas_prior);
+  PARAMETER_VECTOR(betas);
+  dll -= dnorm(betas, betas_prior(0), betas_prior(1), true).sum();
+
+  PARAMETER_VECTOR(log_alpha_prior);
+  vector<Type> alpha_prior = exp(log_alpha_prior);
+
+  PARAMETER(log_alpha);
+  Type alpha = exp(log_alpha);
+  
+  dll -= dgamma(alpha, alpha_prior[0], alpha_prior[1], true);
+
+  PARAMETER_VECTOR(gamma);
+  dll -= dgamma(gamma, alpha, Type(1) / alpha, true).sum(); // mean = 1
+  
+  // AR2
+  DATA_SPARSE_MATRIX(A);
+  PARAMETER_VECTOR(pacf_vec);
+  PARAMETER_VECTOR(yob);
+  dll += ktools::AR2(yob, pacf_vec);
+
+  // Store for criteria
+  vector<Type> eta = X * betas;
+  vector<Type> re = A * yob;
+  vector<Type> lambda = gamma * E * exp(eta + re);
+  
+  // Data likelihood
+  vector<Type> ll(y.size());
+
+  for (int i = 0; i < y.size(); i++)
+  {
+    if (I[i] == 0)
+    {
+      ll[i] = log(dpois(y[i], lambda[i]));
+    }
+    else
+    {
+      if (y[i] == 0)
+        ll[i] = log(dpois(Type(0), lambda[i]));
+      else if (y[i] == 1)
+        ll[i] = log(1 - ppois(Type(0), lambda[i]));
+      else if (y[i] == 98)
+        ll[i] = log(1 - ppois(Type(98), lambda[i]) + DBL_EPSILON);
+    }
+  }
+
+  dll -= ll.sum();
+
+  REPORT(ll);
+  REPORT(betas);
+  REPORT(alpha);
+  REPORT(gamma);
+  REPORT(lambda);
+
+  SIMULATE {
+    vector<Type> y_new = rpois(lambda);
+    REPORT(y_new);
+  }
+
+  return dll; 
+}
